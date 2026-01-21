9609e6b2b63b429e23133b339b85d27269010496
Jeslor Ssozi <jbestius@gmail.com>
2026-01-21 14:36:59 +0100

    add logic to redirect unsigned in user or the one whose profile is not yet complete

--- app/(root)/messages/page.tsx
+++ app/(root)/messages/page.tsx
@@ -1,43 +1,13 @@
-"use client";
+import { getSession } from "@/lib/session/session";
+import { redirect } from "next/navigation";
+import Messages from "@/components/Messages/Messages";
 
-import { useChatSocket } from "@/Hooks/InitializeMessageSocket";
+export default async function MessagesPage() {
+  const session = await getSession();
+  if (!session) redirect("/signin");
+  if (!session.user?.isOnboarded) {
+    redirect(`/profile/${session.user?.id}`);
+  }
 
-import {
-  useFetchMessages,
-  useMessageActions,
-  useMessages,
-  useProfile,
-} from "@/store";
-
-import { useEffect, useState } from "react";
-import Chat from "@/components/Messages/Chat";
-import ChatLoading from "@/components/Messages/ChatLoading";
-
-export default function MessagesPage() {
-  const [hasFetchedMessages, setHasFetchedMessages] = useState(false);
-  const [hasFetchedProfiles, setHasFetchedProfiles] = useState(false);
-
-  const fetchMessages = useFetchMessages();
-  const messages = useMessages();
-
-  const { userProfiles } = useMessageActions();
-  const profile = useProfile();
-
-  /* Fetch messages */
-  useEffect(() => {
-    if (profile?.id && !hasFetchedMessages) {
-      fetchMessages(profile.id);
-      setHasFetchedMessages(true);
-    }
-  }, [profile?.id, hasFetchedMessages, fetchMessages]);
-
-  /* Build user profiles from messages */
-  useEffect(() => {
-    if (hasFetchedMessages && !hasFetchedProfiles && messages.length > 0) {
-      userProfiles();
-      setHasFetchedProfiles(true);
-    }
-  }, [hasFetchedMessages, hasFetchedProfiles, messages]);
-
-  return hasFetchedProfiles && hasFetchedMessages ? <Chat /> : <ChatLoading />;
+  return <Messages />;
 }
